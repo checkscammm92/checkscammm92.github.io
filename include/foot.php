7354aea02afed8d6d01d8c14b07e87de48dc303a
checkscammm92 <111642091+checkscammm92@users.noreply.github.com>
2022-08-20 21:16:35 +0700

    Add files via upload

---
+++ include/foot.php
@@ -0,0 +1,102 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="footer-top">
+                    <div class="row">
+                        <div class="col-md-6 col-lg-3">
+                            <div class="footer-item">
+                                <div class="footer-item_text">
+                                    DATA CENTER ADMIN VIETNAM
+                                </div>
+                                <div class="footer-item_link">
+                                    Điện thoại:
+                                    <a href="#"><?=$site_sdt_momo;?></a>
+                                </div>
+                                <div class="footer-item_link">
+                                    Email:
+                                    <a href="#">admin.vn@admin.com</a>
+                                </div>
+                                <div class="footer-item_link">
+                                    Địa chỉ: VietNam
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-3 col-6">
+                            <div class="footer-item">
+                                <div class="footer-item_text footer-item_decor">
+                                    Dịch vụ Center
+                                </div>
+                                <ul class="footer-item_list">
+                                    <li>
+                                        <a href="/service/rules">Dịch vụ uy tín</a>
+                                    </li>
+                                    <li>
+                                        <a href="/service/rules">Đăng kí quỹ bảo hiểm</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-3 col-6">
+                            <div class="footer-item">
+                                <div class="footer-item_text footer-item_decor">
+                                    Thông tin Admin
+                                </div>
+                                <ul class="footer-item_list">
+                                    <li>
+                                        <a href="news">Tin tức</a>
+                                    </li>
+                                    <li>
+                                        <a href="/">Supper Team Free Fire</a>
+                                    </li>
+                                    <li>
+                                        <a href="/">Supper Team MMO</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 col-lg-3 col-6">
+                            <div class="footer-item">
+                                <div class="footer-item_text footer-item_decor">
+                                    Cộng đồng
+                                </div>
+                                <ul class="list-unstyled mb-0 footer-item_social">
+                                    <li class="facebook">
+                                        <a href="#">
+                                            <i class="fab fa-facebook-f"></i>
+                                        </a>
+                                    </li>
+                                    <li class="telegram">
+                                        <a href="#">
+                                            <i class="fab fa-telegram-plane"></i>
+                                        </a>
+                                    </li>
+                                    <li class="twitter">
+                                        <a href="#">
+                                            <i class="fab fa-twitter"></i>
+                                        </a>
+                                    </li>
+                                    <li class="youtube">
+                                        <a href="#">
+                                            <i class="fab fa-youtube"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<div class="float-buttons">
+    <a href="/service/denounce" class="btn-theme btn-theme_primary">TỐ CÁO <span></span></a>
+    <a href="/service/reputation" class="btn-theme btn-theme_success">CHECK UY TÍN<span></span></a>
+</div>
+
+</body>
+
+</html>
